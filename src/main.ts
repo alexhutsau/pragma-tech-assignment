79600d6123512dac4282420edb30af8449e67297
Alex Hutsau <aleksan9ep@gmail.com>
2021-09-24 11:50:22 +0300

    Setup ConfigModule

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   const swDocument = SwaggerModule.createDocument(app, swOptions)
   SwaggerModule.setup('api/docs', app, swDocument)
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 
 bootstrap();
